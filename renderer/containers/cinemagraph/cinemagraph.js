3dba88ca0f8ca00130357896b1fcde301ee9d907
“zdenham” <zdenham@usc.edu>
2017-12-21 13:26:10 -0800

    up to speed

--- renderer/containers/cinemagraph/cinemagraph.js
+++ renderer/containers/cinemagraph/cinemagraph.js
@@ -2,18 +2,32 @@ import React from "react";
 import NavBar from "../../components/navBar";
 import DrawingCanvas from "../../components/drawingCanvas";
 import Trimmer from "../../components/trimmer";
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import * as Actions from './actions';
 
 
 class Cinemagraph extends React.Component {
 	render() {
-		console.log("PROPS: ", this.props);
 		return (
 			<div>
-				hello still
+				<NavBar
+					selectCinemagraphVideo={this.props.selectCinemagraphVideo}
+					initializeCinemagraphCanvas={this.props.initializeCinemagraphCanvas}
+				/>
+				<DrawingCanvas src={this.props.cinemagraph.videoPath} videoHeight={this.props.cinemagraph.videoHeight} />
+				<Trimmer />
 			</div>
 		);
 	}
 }
 
-export default connect(state => state)(Cinemagraph)
+const mapDispatchToProps = dispatch => {
+  return {
+    initializeCinemagraphCanvas: () => dispatch(Actions.initializeCinemagraphCanvas()),
+    selectCinemagraphVideo: (files) => dispatch(Actions.selectCinemagraphVideo(files)),
+  };
+};
+
+const mapStateToProps = state => ({ cinemagraph: state.cinemagraph });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cinemagraph)
