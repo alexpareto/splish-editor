17cc03d0b349706e7cd184086f310aa29f112099
Alex Pareto <alex.pareto@gmail.com>
2018-01-29 21:19:09 -0800

    add ability to copy from export item (#95)

--- renderer/components/exportItem.js
+++ renderer/components/exportItem.js
@@ -1,30 +1,34 @@
 import * as globalStyles from '../globalStyles';
+import Clipboard from 'react-clipboard.js';
 
 export default props => {
   return (
-    <div>
-      <video autoPlay="true" loop="true" height={props.height}>
-        <source src={props.videoUrl} />
-      </video>
-      <style jsx>{`
-        div {
-          background: ${globalStyles.backgroundAccent};
-          color: ${globalStyles.textColor};
-          border-style: none;
-          box-sizing: border-box;
-          font-family: ${globalStyles.fontFamily};
-          transition: all 0.2s ease;
-          box-shadow: ${globalStyles.lighterBoxShadow};
-          height: 200px;
-          margin: 10px;
-        }
+    <Clipboard component="a" data-clipboard-text={props.shareLink}>
+      <div className="item">
+        <video autoPlay="true" loop="true" height={props.height}>
+          <source src={props.videoUrl} />
+        </video>
+        <style jsx>{`
+          .item {
+            background: ${globalStyles.backgroundAccent};
+            color: ${globalStyles.textColor};
+            border-style: none;
+            box-sizing: border-box;
+            font-family: ${globalStyles.fontFamily};
+            transition: all 0.2s ease;
+            box-shadow: ${globalStyles.lighterBoxShadow};
+            height: 200px;
+            margin: 10px;
+            position: relative;
+          }
 
-        div:hover {
-          box-shadow: ${globalStyles.heavierBoxShadow};
-          transform: translateY(-3px);
-          cursor: pointer;
-        }
-      `}</style>
-    </div>
+          .item:hover {
+            box-shadow: ${globalStyles.heavierBoxShadow};
+            transform: translateY(-3px);
+            cursor: pointer;
+          }
+        `}</style>
+      </div>
+    </Clipboard>
   );
 };
