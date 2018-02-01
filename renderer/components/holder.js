0328348e5c2d562051ce73fcf1032fbd151a526c
Alex Pareto <alex.pareto@gmail.com>
2018-01-31 19:46:52 -0800

    add google analytics (#115)
    * add google analytics
    
    * remove console log

--- renderer/components/holder.js
+++ renderer/components/holder.js
@@ -1,18 +1,31 @@
-export default props => {
-  return (
-    <div className="holder">
-      {props.children}
-      <style jsx>
-        {`
-          .holder {
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            right: 0;
-            left: 0;
-          }
-        `}
-      </style>
-    </div>
-  );
-};
+import React from 'react';
+import { initGA, logPageView } from '../lib/analytics';
+
+export default class Holder extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+
+  render() {
+    return (
+      <div className="holder">
+        {this.props.children}
+        <style jsx>
+          {`
+            .holder {
+              position: absolute;
+              top: 0;
+              bottom: 0;
+              right: 0;
+              left: 0;
+            }
+          `}
+        </style>
+      </div>
+    );
+  }
+}
