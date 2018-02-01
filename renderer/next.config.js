ac5805e1ae20a0745a456ce99adfa1da8ea33762
Alex Pareto <alex.pareto@gmail.com>
2018-02-01 11:53:03 -0800

    Uglify (#140)
    * remove uglify
    
    * bump version
    
    * Temp files and packaged versions (#141)
    
    * temp direcotries and package ffmpeg
    
    * keep temp out of build
    
    * remove apollo

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -10,6 +10,13 @@ module.exports = {
     };
 
     config.target = 'electron-renderer';
+    config.plugins = config.plugins.filter(plugin => {
+      if (plugin.constructor.name === 'UglifyJsPlugin') {
+        return false;
+      } else {
+        return true;
+      }
+    });
 
     return config;
   },
